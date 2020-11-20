ffc2da12616b35cd9aff4976c201bedaf1d76b33
Alexander Werning <awerning@mail.upb.de>
2020-11-21 00:19:38 +0100

    Fix server

--- server.py
+++ server.py
@@ -9,6 +9,19 @@ import sys
 class RequestHandler(SimpleXMLRPCRequestHandler):
     rpc_paths = ('/RPC2',)
 
+class Server(object):
+    def __init__(self, hostport):
+        self.server = SimpleXMLRPCServer(hostport, requestHandler=RequestHandler, allow_none=True)
+
+    def register_function(self, function, name=None):
+        def _function(args, kwargs):
+            return function(*args, **kwargs)
+        _function.__name__ = function.__name__
+        self.server.register_function(_function, name)
+
+    def serve_forever(self):
+        self.server.serve_forever()
+
 
 class DummyObject:
     def _dispatch(self, method, params):
@@ -21,13 +34,23 @@ if __name__ == "__main__":
 
     while True:
         # Create server
-        with SimpleXMLRPCServer(('localhost', 8000), requestHandler=RequestHandler, allow_none=True) as server:
+        with Server(8000) as server:
             if dry_run:
                 k40 = DummyObject()
             else:
                 k40 = K40_CLASS()
 
-            server.register_instance(k40)
+            server.register_function(k40.n_timeouts)
+            server.register_function(k40.timeout)
+            server.register_function(k40.rapid_move)
+            server.register_function(k40.send_data)
+            server.register_function(k40.home_position)
+            server.register_function(k40.reset_usb)
+            server.register_function(k40.pause_un_pause)
+            server.register_function(k40.release_usb)
+            server.register_function(k40.initialize_device)
+            server.register_function(k40.say_hello)
+            server.register_function(k40.unlock_rail)
 
             # Run the server's main loop
             server.serve_forever()
