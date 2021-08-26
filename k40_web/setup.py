43d8007b5c15d8292698838c887791de88fb158c
Alexander Werning <awerning@mail.upb.de>
2021-08-26 20:43:13 +0200

    Add setup.py

---
+++ k40_web/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='k40_web',
+    version='0.0.1',
+    url='https://github.com/alexanderwerning/k40_web.git',
+    author='Alexander Werning',
+    author_email='alexander.werning@gmx.de',
+    description='K40 laser web controller',
+    packages=find_packages(),    
+    install_requires=[],
+)
